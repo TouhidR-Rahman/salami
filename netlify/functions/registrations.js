f7393fda99eed97b576c18052dda8cb7309e3fe1
ahnaftahmidd <ahnaftahmid012@gmail.com>
2026-03-20 15:12:14 +0600

    Add DATABASE_URL validation with helpful error messages

--- netlify/functions/registrations.js
+++ netlify/functions/registrations.js
@@ -12,6 +12,9 @@ function formatSalamiAmount(amount) {
 }
 
 async function getPool() {
+    if (!process.env.DATABASE_URL) {
+        throw new Error('DATABASE_URL environment variable is not set. Please add it to Netlify environment variables.');
+    }
     return new Pool({ connectionString: process.env.DATABASE_URL });
 }
 
